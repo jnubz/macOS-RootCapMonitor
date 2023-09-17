bcc0b5485e7557911d500589ffb1831c51ea1906
Jared Nay <jarednay@mac.com>
2023-09-17 11:47:44 -0400

    Add zsh file.

---
+++ RootCapMonitor.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Get the percentage of disk space used
+space_used=$(df -H / | awk 'NR==2{print $5}')
+
+# Remove the % sign from the percentage
+space_used=${space_used%\%}
+
+# Check if the space used is greater than or equal to 90%
+if [[ $space_used -ge 90 ]]; then
+  echo "Hard drive is almost full. Space used: $space_used%"
+else
+  echo "Hard drive is not full. Space used: $space_used%"
+fi
